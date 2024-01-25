4f6026886962b468a31ae323b7820c555042a89f
enestastan <enest191@gmail.com>
2024-01-25 21:39:20 +0300

    basic starting point for parsing stderr outputs are to observe the hex values of bytestream stdout outputs are to be directed to protoscope

--- CppProtoParser/src/main.cpp
+++ CppProtoParser/src/main.cpp
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include "ParserEntry.pb.h"
 
+static const int32_t BUFFER_SIZE = 100;
+
 int main()
 {
+	Sample::ParserEntry message;
+	uint8_t buffer[BUFFER_SIZE];
 
-	printf("hello\n");
+	message.set_uinteger(150);
+	message.set_fixed64double(151);
+	message.set_fixed32float(152);
+
+	message.SerializeToArray(buffer, BUFFER_SIZE);
+
+	for (int i = 0; i < message.ByteSizeLong(); i++)
+	{
+		std::cout << std::hex << *(buffer + i);
+		// printf("%X", *(buffer + i));
+	}
+
+	for (int i = 0; i < message.ByteSizeLong(); i++)
+	{
+		fprintf(stderr, "%.2X", *(buffer + i));
+	}
+
+	fprintf(stderr, "\n");
+
 	return 0;
 }
