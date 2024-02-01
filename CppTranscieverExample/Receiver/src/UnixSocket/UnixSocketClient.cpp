30f341daed18e793e235cb4c0b75d0aae757667c
enestastan <enest191@gmail.com>
2024-02-01 22:03:26 +0300

    errno added for additional info

--- CppTranscieverExample/Receiver/src/UnixSocket/UnixSocketClient.cpp
+++ CppTranscieverExample/Receiver/src/UnixSocket/UnixSocketClient.cpp
@@ -1,31 +1,29 @@
 #include "UnixSocketClient.h"
 
-UnixSocketClient::UnixSocketClient()
-{
-};
+#include "errno.h"
+#include "stdio.h"
+
+UnixSocketClient::UnixSocketClient(){};
 
 UnixSocketClient::~UnixSocketClient() {
 	close(m_clientSocket);
 }
 
 bool UnixSocketClient::initialize() {
-
-	if((m_clientSocket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+	if ((m_clientSocket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
 		printf("Creating socket failed \n");
 		return false;
 	}
 
-	
 	return true;
 }
 
 bool UnixSocketClient::connect(const std::string& sSocketFilePath) {
-
 	m_serverAddress.sun_family = AF_UNIX;
-	strncpy( m_serverAddress.sun_path, sSocketFilePath.c_str() , sSocketFilePath.size());
-	uint32_t uAddressLength = sSocketFilePath.size() + sizeof(m_serverAddress.sun_len)+ sizeof(m_serverAddress.sun_family);
+	strncpy(m_serverAddress.sun_path, sSocketFilePath.c_str(), sSocketFilePath.size());
+	uint32_t uAddressLength = sSocketFilePath.size() + sizeof(m_serverAddress.sun_len) + sizeof(m_serverAddress.sun_family);
 
-	if(::connect(m_clientSocket, (struct sockaddr*)&m_serverAddress, uAddressLength) == -1 ) {
+	if (::connect(m_clientSocket, (struct sockaddr*)&m_serverAddress, uAddressLength) == -1) {
 		printf("Error on connect() call \n");
 		return false;
 	}
@@ -35,8 +33,8 @@ bool UnixSocketClient::connect(const std::string& sSocketFilePath) {
 }
 
 bool UnixSocketClient::send(std::uint8_t* pBuffer, std::size_t uSize) {
-	if( ::send(m_clientSocket, pBuffer, uSize, 0) == -1 ) {
-		printf("Send error!\n");
+	if (::send(m_clientSocket, pBuffer, uSize, 0) <= 0) {
+		printf("Send error! errno: %d - %s\n", errno, strerror(errno));
 		return false;
 	}
 
@@ -45,8 +43,8 @@ bool UnixSocketClient::send(std::uint8_t* pBuffer, std::size_t uSize) {
 
 std::size_t UnixSocketClient::receive(std::uint8_t* pBuffer, std::size_t uSize) {
 	ssize_t nReceivedSize;
-	if((nReceivedSize = ::recv(m_clientSocket, pBuffer, uSize, 0)) <= 0) {
-		printf("Receiver error!\n");
+	if ((nReceivedSize = ::recv(m_clientSocket, pBuffer, uSize, 0)) <= 0) {
+		printf("Receive error! errno: %d - %s\n", errno, strerror(errno));
 		return 0;
 	}
 
--- CppTranscieverExample/Transmitter/src/UnixSocket/UnixSocketServer.cpp
+++ CppTranscieverExample/Transmitter/src/UnixSocket/UnixSocketServer.cpp
@@ -1,9 +1,6 @@
 #include "UnixSocketServer.h"
 
-UnixSocketServer::UnixSocketServer(const std::string& socketFilePath) :
-	m_SOCKET_FILE_PATH { socketFilePath }
-{
-};
+UnixSocketServer::UnixSocketServer(const std::string& socketFilePath) : m_SOCKET_FILE_PATH{socketFilePath} {};
 
 UnixSocketServer::~UnixSocketServer() {
 	close(m_clientSocket);
@@ -11,23 +8,22 @@ UnixSocketServer::~UnixSocketServer() {
 }
 
 bool UnixSocketServer::initialize() {
-
-	if((m_serverSocket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+	if ((m_serverSocket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
 		printf("Creating socket failed \n");
 		return false;
 	}
 
 	m_serverAddress.sun_family = AF_UNIX;
-	strncpy( m_serverAddress.sun_path, m_SOCKET_FILE_PATH.c_str(), m_SOCKET_FILE_PATH.size() );
+	strncpy(m_serverAddress.sun_path, m_SOCKET_FILE_PATH.c_str(), m_SOCKET_FILE_PATH.size());
 	unlink(m_SOCKET_FILE_PATH.c_str());
-	uint32_t uAddressLength = m_SOCKET_FILE_PATH.size() + sizeof(m_serverAddress.sun_len)+ sizeof(m_serverAddress.sun_family);
+	uint32_t uAddressLength = m_SOCKET_FILE_PATH.size() + sizeof(m_serverAddress.sun_len) + sizeof(m_serverAddress.sun_family);
 
-	if( bind(m_serverSocket, (struct sockaddr*)&m_serverAddress, uAddressLength) != 0) {
+	if (bind(m_serverSocket, (struct sockaddr*)&m_serverAddress, uAddressLength) != 0) {
 		printf("Error on binding socket \n");
 		return false;
 	}
 
-	if( listen(m_serverSocket, 1) != 0 ) {
+	if (listen(m_serverSocket, 1) != 0) {
 		printf("Error on listen call \n");
 		return false;
 	}
@@ -39,7 +35,7 @@ bool UnixSocketServer::acceptConnection() {
 	printf("Waiting for connection.... \n");
 
 	unsigned int nSocketAddressLength = 0;
-	if( (m_clientSocket = accept(m_serverSocket, (struct sockaddr*)&m_clientAddress, &nSocketAddressLength)) == -1 ) {
+	if ((m_clientSocket = accept(m_serverSocket, (struct sockaddr*)&m_clientAddress, &nSocketAddressLength)) == -1) {
 		printf("Error on accept() call \n");
 		return false;
 	}
@@ -49,8 +45,8 @@ bool UnixSocketServer::acceptConnection() {
 }
 
 bool UnixSocketServer::send(std::uint8_t* pBuffer, std::size_t uSize) {
-	if( ::send(m_clientSocket, pBuffer, uSize, 0) == -1 ) {
-		printf("Send error!\n");
+	if (::send(m_clientSocket, pBuffer, uSize, 0) <= 0) {
+		printf("Send error! errno: %d - %s\n", errno, strerror(errno));
 		return false;
 	}
 
@@ -59,8 +55,8 @@ bool UnixSocketServer::send(std::uint8_t* pBuffer, std::size_t uSize) {
 
 std::size_t UnixSocketServer::receive(std::uint8_t* pBuffer, std::size_t uSize) {
 	ssize_t nReceivedSize;
-	if((nReceivedSize = ::recv(m_clientSocket, pBuffer, uSize, 0)) <= 0) {
-		printf("Receiver error!\n");
+	if ((nReceivedSize = ::recv(m_clientSocket, pBuffer, uSize, 0)) <= 0) {
+		printf("Receive error! errno: %d - %s\n", errno, strerror(errno));
 		return 0;
 	}
 
